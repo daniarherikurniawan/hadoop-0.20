2acb9677464fb816b25632bfbad07a50b1ea1fa3
Daniar Heri Kurniawan <ddhhkk2@gmail.com>
2017-04-12 05:31:40 +0700

    update the latest code

--- mapredscript.sh
+++ mapredscript.sh
@@ -6,51 +6,54 @@ export JAVA_HOME=/usr/lib/jvm/java-8-oracle/jre/
 export HADOOP_HOME=/users/daniar/hadoop
 
 cd hadoop/
+git pull ucare-github-dan master
 
-echo ..... Saving gutenberg files to HDFS
-bin/hadoop dfs -copyFromLocal gutenberg /gutenberg
-echo       Folder gutenberg is copied to HDFS
-bin/hadoop dfs -copyFromLocal gutenberg /gutenberg0
-echo       Folder gutenberg0 is copied to HDFS
-bin/hadoop dfs -copyFromLocal gutenberg /gutenberg1
-echo       Folder gutenberg1 is copied to HDFS
-bin/hadoop dfs -copyFromLocal gutenberg /gutenberg2
-echo       Folder gutenberg2 is copied to HDFS
-bin/hadoop dfs -copyFromLocal gutenberg /gutenberg3
-echo       Folder gutenberg3 is copied to HDFS
-bin/hadoop dfs -copyFromLocal gutenberg /gutenberg4
-echo       Folder gutenberg4 is copied to HDFS
-bin/hadoop dfs -copyFromLocal gutenberg /gutenberg5
-echo       Folder gutenberg5 is copied to HDFS
-bin/hadoop dfs -copyFromLocal gutenberg /gutenberg6
-echo       Folder gutenberg6 is copied to HDFS
-bin/hadoop dfs -copyFromLocal gutenberg /gutenberg7
-echo       Folder gutenberg7 is copied to HDFS
-bin/hadoop dfs -copyFromLocal gutenberg /gutenberg8
-echo       Folder gutenberg8 is copied to HDFS
-bin/hadoop dfs -copyFromLocal gutenberg /gutenberg9
-echo       Folder gutenberg9 is copied to HDFS
-bin/hadoop dfs -copyFromLocal gutenberg /gutenberg10
-echo       Folder gutenberg10 is copied to HDFS
-bin/hadoop dfs -copyFromLocal gutenberg /gutenberg11
-echo       Folder gutenberg11 is copied to HDFS
-bin/hadoop dfs -copyFromLocal gutenberg /gutenberg12
-echo       Folder gutenberg12 is copied to HDFS
-bin/hadoop dfs -copyFromLocal gutenberg /gutenberg13
-echo       Folder gutenberg13 is copied to HDFS
-bin/hadoop dfs -copyFromLocal gutenberg /gutenberg14
-echo       Folder gutenberg14 is copied to HDFS
-bin/hadoop dfs -copyFromLocal gutenberg /gutenberg15
-echo       Folder gutenberg15 is copied to HDFS
-bin/hadoop dfs -copyFromLocal gutenberg /gutenberg16
-echo       Folder gutenberg16 is copied to HDFS
-bin/hadoop dfs -copyFromLocal gutenberg /gutenberg17
-echo       Folder gutenberg17 is copied to HDFS
-bin/hadoop dfs -copyFromLocal gutenberg /gutenberg18
-echo ..... Finish saving gutenberg files to HDFS
+fileName=gutenbergX
+
+echo ..... Saving $fileName files to HDFS
+bin/hadoop dfs -copyFromLocal $fileName /$fileName
+echo       Folder $fileName is copied to HDFS
+bin/hadoop dfs -copyFromLocal $fileName /$fileName0
+echo       Folder $fileName0 is copied to HDFS
+bin/hadoop dfs -copyFromLocal $fileName /$fileName1
+echo       Folder $fileName1 is copied to HDFS
+bin/hadoop dfs -copyFromLocal $fileName /$fileName2
+echo       Folder $fileName2 is copied to HDFS
+bin/hadoop dfs -copyFromLocal $fileName /$fileName3
+echo       Folder $fileName3 is copied to HDFS
+bin/hadoop dfs -copyFromLocal $fileName /$fileName4
+echo       Folder $fileName4 is copied to HDFS
+bin/hadoop dfs -copyFromLocal $fileName /$fileName5
+echo       Folder $fileName5 is copied to HDFS
+bin/hadoop dfs -copyFromLocal $fileName /$fileName6
+echo       Folder $fileName6 is copied to HDFS
+bin/hadoop dfs -copyFromLocal $fileName /$fileName7
+echo       Folder $fileName7 is copied to HDFS
+bin/hadoop dfs -copyFromLocal $fileName /$fileName8
+echo       Folder $fileName8 is copied to HDFS
+bin/hadoop dfs -copyFromLocal $fileName /$fileName9
+echo       Folder $fileName9 is copied to HDFS
+bin/hadoop dfs -copyFromLocal $fileName /$fileName10
+echo       Folder $fileName10 is copied to HDFS
+bin/hadoop dfs -copyFromLocal $fileName /$fileName11
+echo       Folder $fileName11 is copied to HDFS
+bin/hadoop dfs -copyFromLocal $fileName /$fileName12
+echo       Folder $fileName12 is copied to HDFS
+bin/hadoop dfs -copyFromLocal $fileName /$fileName13
+echo       Folder $fileName13 is copied to HDFS
+bin/hadoop dfs -copyFromLocal $fileName /$fileName14
+echo       Folder $fileName14 is copied to HDFS
+bin/hadoop dfs -copyFromLocal $fileName /$fileName15
+echo       Folder $fileName15 is copied to HDFS
+bin/hadoop dfs -copyFromLocal $fileName /$fileName16
+echo       Folder $fileName16 is copied to HDFS
+bin/hadoop dfs -copyFromLocal $fileName /$fileName17
+echo       Folder $fileName17 is copied to HDFS
+bin/hadoop dfs -copyFromLocal $fileName /$fileName18
+echo ..... Finish saving $fileName files to HDFS
 echo  
 echo ..... Start MapRed Job
-# bin/hadoop jar build/hadoop-examples-0.20.3-SNAPSHOT.jar wordcount /gutenberg /gutenberg-output
+# bin/hadoop jar build/hadoop-examples-0.20.3-SNAPSHOT.jar wordcount /$fileName /$fileName-output
 echo ..... Finish MapRed Job
 echo  
 echo HDFS directory
--- myscript.sh
+++ myscript.sh
@@ -15,35 +15,35 @@ git commit -m "update the latest code" -a
 git push origin master
 echo Finished updating git codes!
 
-projURI=hadoopcluster.cs331-uc.emulab.net
+# projURI=hadoopcluster.cs331-uc.emulab.net
 
-counter=0
-while [ $counter -lt $maxNodes ]
-do
-	host=node-$counter.$projURI
-	echo 
-	echo node: $host
-	echo ...... START Scanning ssh fingerprint 
-	ssh-keygen -R $host
-	ssh-keyscan $host >> ~/.ssh/known_hosts
-	echo ...... END Scanning ssh fingerprint 
+# counter=0
+# while [ $counter -lt $maxNodes ]
+# do
+# 	host=node-$counter.$projURI
+# 	echo 
+# 	echo node: $host
+# 	echo ...... START Scanning ssh fingerprint 
+# 	ssh-keygen -R $host
+# 	ssh-keyscan $host >> ~/.ssh/known_hosts
+# 	echo ...... END Scanning ssh fingerprint 
 
-	if [ $counter -eq 0 ]
-	then
-		echo ...... Starting masterscript
-		ssh $host 'bash -s' < masterscript.sh $maxNodes
-	else
-		echo ...... Starting slavescript
-		ssh $host 'bash -s' < slavescript.sh $maxNodes
-	fi
+# 	if [ $counter -eq 0 ]
+# 	then
+# 		echo ...... Starting masterscript
+# 		ssh $host 'bash -s' < masterscript.sh $maxNodes
+# 	else
+# 		echo ...... Starting slavescript
+# 		ssh $host 'bash -s' < slavescript.sh $maxNodes
+# 	fi
 
-	echo ...... Finished running nodescript
+# 	echo ...... Finished running nodescript
 
-	((counter++))
-done
-echo  
-echo ...... Starting MAP REDUCE Test
-host=node-0.$projURI
+# 	((counter++))
+# done
+# echo  
+# echo ...... Starting MAP REDUCE Test
+host=node-1.$projURI
 ssh $host 'bash -s' < mapredscript.sh 
 echo open:
 echo 	$host:50070 
