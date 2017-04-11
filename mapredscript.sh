aae5ee94058236cdda79dea1fee01bef0fe4f12d
Daniar Heri Kurniawan <ddhhkk2@gmail.com>
2017-04-12 05:43:32 +0700

    update the latest code

--- mapredscript.sh
+++ mapredscript.sh
@@ -6,54 +6,54 @@ export JAVA_HOME=/usr/lib/jvm/java-8-oracle/jre/
 export HADOOP_HOME=/users/daniar/hadoop
 
 cd hadoop/
-git pull ucare-github-dan master
+# git pull ucare-github-dan master
 
 fileName=gutenbergX
-
+destination=/$fileName0
 echo ..... Saving $fileName files to HDFS
-bin/hadoop dfs -copyFromLocal $fileName /$fileName
+bin/hadoop dfs -copyFromLocal $fileName $destination
 echo       Folder $fileName is copied to HDFS
-bin/hadoop dfs -copyFromLocal $fileName /$fileName0
+bin/hadoop dfs -copyFromLocal $fileName $destination0
 echo       Folder $fileName0 is copied to HDFS
-bin/hadoop dfs -copyFromLocal $fileName /$fileName1
+bin/hadoop dfs -copyFromLocal $fileName $destination1
 echo       Folder $fileName1 is copied to HDFS
-bin/hadoop dfs -copyFromLocal $fileName /$fileName2
+bin/hadoop dfs -copyFromLocal $fileName $destination2
 echo       Folder $fileName2 is copied to HDFS
-bin/hadoop dfs -copyFromLocal $fileName /$fileName3
+bin/hadoop dfs -copyFromLocal $fileName $destination3
 echo       Folder $fileName3 is copied to HDFS
-bin/hadoop dfs -copyFromLocal $fileName /$fileName4
+bin/hadoop dfs -copyFromLocal $fileName $destination4
 echo       Folder $fileName4 is copied to HDFS
-bin/hadoop dfs -copyFromLocal $fileName /$fileName5
+bin/hadoop dfs -copyFromLocal $fileName $destination5
 echo       Folder $fileName5 is copied to HDFS
-bin/hadoop dfs -copyFromLocal $fileName /$fileName6
+bin/hadoop dfs -copyFromLocal $fileName $destination6
 echo       Folder $fileName6 is copied to HDFS
-bin/hadoop dfs -copyFromLocal $fileName /$fileName7
+bin/hadoop dfs -copyFromLocal $fileName $destination7
 echo       Folder $fileName7 is copied to HDFS
-bin/hadoop dfs -copyFromLocal $fileName /$fileName8
+bin/hadoop dfs -copyFromLocal $fileName $destination8
 echo       Folder $fileName8 is copied to HDFS
-bin/hadoop dfs -copyFromLocal $fileName /$fileName9
+bin/hadoop dfs -copyFromLocal $fileName $destination9
 echo       Folder $fileName9 is copied to HDFS
-bin/hadoop dfs -copyFromLocal $fileName /$fileName10
+bin/hadoop dfs -copyFromLocal $fileName $destination10
 echo       Folder $fileName10 is copied to HDFS
-bin/hadoop dfs -copyFromLocal $fileName /$fileName11
+bin/hadoop dfs -copyFromLocal $fileName $destination11
 echo       Folder $fileName11 is copied to HDFS
-bin/hadoop dfs -copyFromLocal $fileName /$fileName12
+bin/hadoop dfs -copyFromLocal $fileName $destination12
 echo       Folder $fileName12 is copied to HDFS
-bin/hadoop dfs -copyFromLocal $fileName /$fileName13
+bin/hadoop dfs -copyFromLocal $fileName $destination13
 echo       Folder $fileName13 is copied to HDFS
-bin/hadoop dfs -copyFromLocal $fileName /$fileName14
+bin/hadoop dfs -copyFromLocal $fileName $destination14
 echo       Folder $fileName14 is copied to HDFS
-bin/hadoop dfs -copyFromLocal $fileName /$fileName15
+bin/hadoop dfs -copyFromLocal $fileName $destination15
 echo       Folder $fileName15 is copied to HDFS
-bin/hadoop dfs -copyFromLocal $fileName /$fileName16
+bin/hadoop dfs -copyFromLocal $fileName $destination16
 echo       Folder $fileName16 is copied to HDFS
-bin/hadoop dfs -copyFromLocal $fileName /$fileName17
+bin/hadoop dfs -copyFromLocal $fileName $destination17
 echo       Folder $fileName17 is copied to HDFS
-bin/hadoop dfs -copyFromLocal $fileName /$fileName18
+bin/hadoop dfs -copyFromLocal $fileName $destination18
 echo ..... Finish saving $fileName files to HDFS
 echo  
 echo ..... Start MapRed Job
-# bin/hadoop jar build/hadoop-examples-0.20.3-SNAPSHOT.jar wordcount /$fileName /$fileName-output
+# bin/hadoop jar build/hadoop-examples-0.20.3-SNAPSHOT.jar wordcount $destination $destination-output
 echo ..... Finish MapRed Job
 echo  
 echo HDFS directory
