14a2aa926cae260d4b60d1331d0872f6b691b7cc
Daniar Heri Kurniawan <ddhhkk2@gmail.com>
2017-04-11 16:43:47 +0700

    update code

--- mapredscript.sh
+++ mapredscript.sh
@@ -9,19 +9,28 @@ cd hadoop/
 
 echo ..... Saving gutenberg files to HDFS
 bin/hadoop dfs -copyFromLocal gutenberg /gutenberg
+echo       Folder gutenberg0 is copied to HDFS
 bin/hadoop dfs -copyFromLocal gutenberg /gutenberg0
+echo       Folder gutenberg1 is copied to HDFS
 bin/hadoop dfs -copyFromLocal gutenberg /gutenberg1
+echo       Folder gutenberg2 is copied to HDFS
 bin/hadoop dfs -copyFromLocal gutenberg /gutenberg2
+echo       Folder gutenberg3 is copied to HDFS
 bin/hadoop dfs -copyFromLocal gutenberg /gutenberg3
+echo       Folder gutenberg4 is copied to HDFS
 bin/hadoop dfs -copyFromLocal gutenberg /gutenberg4
+echo       Folder gutenberg5 is copied to HDFS
 bin/hadoop dfs -copyFromLocal gutenberg /gutenberg5
+echo       Folder gutenberg6 is copied to HDFS
 bin/hadoop dfs -copyFromLocal gutenberg /gutenberg6
+echo       Folder gutenberg7 is copied to HDFS
 bin/hadoop dfs -copyFromLocal gutenberg /gutenberg7
+echo       Folder gutenberg8 is copied to HDFS
 bin/hadoop dfs -copyFromLocal gutenberg /gutenberg8
 echo ..... Finish saving gutenberg files to HDFS
 echo  
 echo ..... Start MapRed Job
-bin/hadoop jar build/hadoop-examples-0.20.3-SNAPSHOT.jar wordcount /gutenberg /gutenberg-output
+# bin/hadoop jar build/hadoop-examples-0.20.3-SNAPSHOT.jar wordcount /gutenberg /gutenberg-output
 echo ..... Finish MapRed Job
 echo  
 echo HDFS directory
