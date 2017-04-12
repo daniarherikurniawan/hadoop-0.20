0b430f1e68baa6ee8d011cc9d24a95385457f367
Daniar Heri Kurniawan <ddhhkk2@gmail.com>
2017-04-13 02:39:06 +0700

    update the latest code

--- src/hdfs/org/apache/hadoop/hdfs/server/datanode/DataNode.java
+++ src/hdfs/org/apache/hadoop/hdfs/server/datanode/DataNode.java
@@ -753,7 +753,7 @@ public class DataNode extends Configured
         }
 
         // DAN: Delay first FBR
-        int delayFirstFBR = 360*1000; // in millisecs
+        int delayFirstFBR = 180*1000; // in millisecs
 
         // send block report
         if (
